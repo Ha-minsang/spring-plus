647801fa27f13db817ba8e8866598acc9fe19200
Ha-minsang <gkalstkd@gmail.com>
2025-12-18 16:43:18 +0900

    Lv2. 쿼리 dsl 구현

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryImpl.java
@@ -0,0 +1,33 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.example.expert.domain.todo.entity.QTodo;
+import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+import static org.example.expert.domain.todo.entity.QTodo.todo;
+import static org.example.expert.domain.user.entity.QUser.user;
+
+@Repository
+public class TodoRepositoryImpl implements TodoRepositoryCustom {
+
+    private JPAQueryFactory queryFactory;
+
+    public TodoRepositoryImpl(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId) {
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(todo)
+                        .leftJoin(todo.user, user).fetchJoin()
+                        .where(todo.id.eq(todoId))
+                        .fetchOne()
+        );
+    }
+}
